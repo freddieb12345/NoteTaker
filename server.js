a5ef3c27d57ad83cb25b67c9427cc1e277b36a49
Freddie Brewin <freddiebrewin@live.com>
2021-05-15 15:00:12 +0100

    setting up body parsing and static

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-//Import all required modules
+//Import all required dependencies
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const database = require("./db/db.json")
+
+//Setting up Express
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//Setting up body parsing and static
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
