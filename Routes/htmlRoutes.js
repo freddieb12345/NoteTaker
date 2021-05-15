65196f3e172a6d2a810f286873f392852c69060f
Freddie Brewin <freddiebrewin@live.com>
2021-05-15 15:45:31 +0100

    Adding htmlRoutes code

---
+++ Routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+//Import dependencies  
+const path = require("path");
+
+//Defining the routing. Shows the user either of the HTML content when they visit the page.
+module.exports = function (app){
+    app.get('.notes', function(req,res) {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+    app.get('/', function(req,res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+}
